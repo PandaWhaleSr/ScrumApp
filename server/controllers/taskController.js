d2d068d12ba6c7db8564292bdb08834b07c2f221
Alexis <sixela295@gmail.com>
2021-11-22 14:47:08 -0500

    task creation functionality added

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -6,17 +6,31 @@ const jwt = require('jsonwebtoken')
 const taskController = {};
 
 taskController.createTask = async (req, res, next) => {
-  console.log('1');
   const { description, status, priority, points, projectId } = req.body
   try {
-    const SQL = `INSERT into tasks (description, status, priority, points, projectId) 
-    VALUES ($1, $2, $3 $4, $5)`;
+    const SQL = `INSERT into tasks (description, status, priorities, points, projectId) 
+    VALUES ($1, $2, $3, $4, $5)`;
     const params = [description, status, priority, points, projectId];    
     const results = await db.query(SQL, params);
     return next();
   } catch (error) {
     return next({log: err})    
   }
+  return next();
+}
+
+taskController.viewTasks = async (req, res, next) => {
+  console.log('Entering inside view Task controller');
+  try {
+    const SQL = `SELECT * from tasks where projectId =1`;   
+    const results = await db.query(SQL);
+    console.log("results contents", results);
+    res.locals.tasks = results.rows;
+    return next();
+  } catch (error) {
+    return next({log: err})    
+  }
+  return next();
 
 }
 
--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -9,4 +9,9 @@ router.post('/newTask', taskController.createTask,  (req, res) => {
   res.status(200).json('Task created under project xyz')
 })
 
+router.get('/viewTasks', taskController.viewTasks,  (req, res) => {
+  console.log('Entering inside View task task router');
+  res.status(200).json(res.locals.tasks);
+})
+
 module.exports = router;
